4645ae9d012e0ea4b06313d6021d1379cca1a68a
Benjamin Mellon <97412534+Cantaloupe2@users.noreply.github.com>
2024-04-07 19:39:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,11 +62,11 @@ def advanced_find(name,title,auths_db, titles_db, auth_train,title_train,indexed
     matching_data.index = top_sim
     matching_data.index.name = "Similarity Score"
     select_list = matching_data.loc[:,['author_x','title']]
+    st.write(select_list)
     for i, val in select_list.iterrows():
         select_list.loc[i,'author_x'] = select_list.loc[i,'author_x']
         st.write(select_list.loc[i,'author_x'][0])
         select_list.loc[i,'title'] = select_list.loc[i,'title']
-    st.write(select_list)
     st.write(matching_data)
     st.selectbox("Select An Author", select_list)
     return path
