4253d0be0d4ceb2ddb4a0abc78953a17544efdbc
Benjamin Mellon <97412534+Cantaloupe2@users.noreply.github.com>
2024-04-07 20:02:11 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,8 +64,8 @@ def advanced_find(name,title,auths_db, titles_db, auth_train,title_train,indexed
     select_list = matching_data.loc[:,['author_x','title']]
     st.write(select_list)
     for i, val in select_list.iterrows():
-        st.write(isinstance(select_list.loc[i,'author_x'],str))
-        if isinstance(select_list.loc[i,'author_x'],str):
+        st.write(len(select_list.loc[i,'author_x']))
+        if True:
             select_list.loc[i,'author_x'] = select_list.loc[i,'author_x']
             select_list.loc[i,'title'] = select_list.loc[i,'title']
             st.write(select_list.loc[i,'author_x'])
