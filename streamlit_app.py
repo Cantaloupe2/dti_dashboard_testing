f9147ef05551a9e76826a8cb30e7af466026ee4c
Benjamin Mellon <97412534+Cantaloupe2@users.noreply.github.com>
2024-04-07 21:17:33 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,24 +39,24 @@ def plot_map(my_row):
     folium_static(m)
 
 def advanced_find(name,title,auths_db, titles_db, auth_train,title_train,indexed_journeys_df):
+    # tranform name and title guess to tfidf vector
     name_vector = auth_train.transform([name])
     title_vector = title_train.transform([title])
-    #power_search = st.text_input("Search Papers by Title")
-    #power_vector = trainer.transform([power_search])
-    # word_list = power_search.split(" ") 
-    # word_vector = trainer.transform(word_list) 
-    # st.write(word_vector)
-    # find the cosine similarity between the power search and the first 200 papers
+    
+    # find the cosine similarity for for both vectors
     cos_sim_auth = cosine_similarity(name_vector, auths_db)
     cos_sim_title = cosine_similarity(title_vector, titles_db)
-    # find the index of the paper with the highest cosine similarity
-    cos_sim = cos_sim_auth/cos_sim_auth.mean() + cos_sim_title/cos_sim_title.mean()
+    
+    # find the index of the paper with the highest combined cosine similarity
+    cos_sim = cos_sim_auth + cos_sim_title
     matching_index = cos_sim.argmax()
+    
     # find the indexis of the top 10 papers with the highest cosine similarity
     top_ten = cos_sim.argsort()[0][-10:]
     top_ten = top_ten[::-1]
     top_sim = cos_sim[0][top_ten]
-    # find the title of the paper with the highest cosine similarity
+    
+    # create a selectbox to select from top results
     matching_data = indexed_journeys_df.iloc[top_ten,:]
     path = indexed_journeys_df.index[matching_index]
     select_list = matching_data.loc[:,['author_x','title']]
